5b892f9f315aedf07b33ffa89fe7d4cf7d175758
kerasis <kseniya.zaika.2004@gmail.com>
2024-03-03 16:42:11 +0200

    usage strings and numerics

--- lab1/code/index.php
+++ lab1/code/index.php
@@ -1,4 +1,7 @@
 <?php
+/*
+ *  буду использовать теги <br> тк \n не отображается в браузере :(((((((
+ */
 /* 1 task */
 /* Imagine a lot of code here */
 $very_bad_unclear_name = "15 chicken wings";
@@ -8,4 +11,23 @@ $order = &$very_bad_unclear_name; // объявление по ссылке
 $order .= ' and cola<3'; // оператор контакенации строк .
 
 // Don't change the line below
-echo "\nYour order is: $very_bad_unclear_name.";
+echo "<br>Your order is: $very_bad_unclear_name.";
+echo "<br>";
+
+// task 2
+$varOneInt = 12345;
+echo "<br>VarOneInt: $varOneInt";
+
+$varTwoFloat = 123.45;
+echo "<br>VarTwoFloat: $varTwoFloat";
+
+echo "<br>Num 12: ";
+echo 11 + 2 - 1;
+
+$lastMonth = 1187.23;
+$thisMonth = 1089.98;
+echo "<br> Разница в расходах: ";
+echo $lastMonth - $thisMonth;
+
+
+
